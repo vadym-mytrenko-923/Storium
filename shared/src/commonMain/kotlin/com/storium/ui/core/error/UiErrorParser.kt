f59a83719abe9d49dc03bd2d5a39a11197c15d99
Vadym Mytrenko <vadym.mytrenko@ninetwothree.co>
2026-05-21 18:33:10 +0300

    [Feature] Detekt

--- shared/src/commonMain/kotlin/com/storium/ui/core/error/UiErrorParser.kt
+++ shared/src/commonMain/kotlin/com/storium/ui/core/error/UiErrorParser.kt
@@ -5,14 +5,14 @@ import com.storium.ui.core.error.model.UiError
 import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.plugins.ServerResponseException
 import storium.shared.generated.resources.Res
-import storium.shared.generated.resources.errorLoginFailed
 import storium.shared.generated.resources.errorNetwork
 import storium.shared.generated.resources.errorServer
 import storium.shared.generated.resources.errorUnknown
+import storium.shared.generated.resources.loginErrorFailed
 
 class UiErrorParser {
     fun parseError(error: Throwable): UiError = when (error) {
-        is ClientRequestException -> UiError(messageResource = AppStringResource(Res.string.errorLoginFailed))
+        is ClientRequestException -> UiError(messageResource = AppStringResource(Res.string.loginErrorFailed))
         is ServerResponseException -> UiError(messageResource = AppStringResource(Res.string.errorServer))
         is kotlinx.io.IOException -> UiError(messageResource = AppStringResource(Res.string.errorNetwork))
         else -> UiError(messageResource = AppStringResource(Res.string.errorUnknown))
